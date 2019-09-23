1c2d15ca4b871fba1fe60a2a3886e64a9f6e218b
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-09-23 18:58:10 +0200

    Add link to fonts, add new operations

--- client/index.js
+++ client/index.js
@@ -1,42 +1,109 @@
 window.onload = function() {
     document.getElementById("insert_person").onclick = () => insertPerson();
+    document.getElementById("show_people").onclick = () => showPeople();
 }
 
 function insertPerson() {
-    var inputDiv = document.getElementById("user_input");
-    inputDiv.innerHTML = "";
-    var form = document.createElement("form");
-    form.method = "POST";
-    form.enctype = "multipart/form-data";
-    form.classList.add("input_form");
+    clearPage();
+    var form = document.getElementById("input_form");
+    form.classList.remove("hidden");
     var cfInput = createTextInput("cf", "Codice fiscale");
     var nameInput = createTextInput("name", "Nome");
     var surnameInput = createTextInput("surname", "Cognome");
     var button = document.createElement("button");
     button.type = "button";
     button.innerHTML = "Inserisci";
-    button.onclick = sendInsertData(form, "add_person");
     form.appendChild(cfInput);
     form.appendChild(nameInput);
     form.appendChild(surnameInput);
     form.appendChild(button);
-    inputDiv.appendChild(form);
+    button.onclick = () => sendInsertData("input_form", "insert_person");
 }
 
-function sendInsertData(form, route) {
+function showPeople() {
+    clearPage();
+    var form = document.getElementById("filters_form");
+    form.classList.remove("hidden");
+    // TODO: apply filters
+    createFiltersCheckBoxes(form, ["partners", "Soci"], ["cutters", "Operatori intagliatrice"], ["printers", "Operatori stampante"]);
+    var resultDiv = document.getElementById("result");
+    var resultList = document.getElementById("result_list");
+    resultList.classList.remove("hidden");
+    fetch(window.location.protocol + "//" + window.location.host.toString() + "/" + "people").then(function(response) {
+        if (response.ok) {
+            response.json().then(jsonResponse => {
+                if (checkIfJsonIsError(jsonResponse) && jsonResponse["response"]["code"] != "200") {
+                    var error = document.createElement("P");
+                    error.innerHTML = jsonResponse["response"]["message"];
+                    error.classList.add("error");
+                    resultDiv.appendChild(error);
+                } else if (!checkIfJsonIsError(jsonResponse)) {
+                    for (const index in jsonResponse) {
+                        var listElem = document.createElement("li");
+                        var person = jsonResponse[index];
+                        listElem.classList.add("result_elem");
+                        listElem.innerHTML = person._personNome + " " + person._personCognome + " -- " + person._personCf;
+                        resultList.appendChild(listElem);
+                    }
+                }
+            });
+        }
+    });        
+}
+
+function clearPage() {
+    var hideableElements = document.getElementsByClassName("hideable");
+    for (var i = 0; i < hideableElements.length; i++) {
+        hideableElements[i].classList.add("hidden");
+        while (hideableElements[i].firstChild) {
+            hideableElements[i].removeChild(hideableElements[i].firstChild);
+        }
+    }
+}
+
+function createFiltersCheckBoxes(form, ...checkBoxes) {
+    checkBoxes.forEach(box => {
+        var input = document.createElement("input");
+        var label = document.createElement("label");
+        input.type = "radio";
+        input.name = "radio_" + form.id;
+        input.id = box[0];
+        label.for = input.id;
+        label.innerHTML = box[1];
+        form.appendChild(input);
+        form.appendChild(label);
+        form.appendChild(document.createElement("br"));
+    });
+}
+
+function sendInsertData(formId, route) {
+    var form = document.getElementById(formId);
     var fd = new FormData(form);
     var post = {
         method: "POST",
-        body: form
+        body: fd
     };
-    fetch(window.location.href.toString() + route, post).then(function(response) {
+    var errors = form.getElementsByClassName("error");
+    while (errors.length > 0) {
+        errors.item(0).parentNode.removeChild(errors.item(0));
+    }
+    var oks = form.getElementsByClassName("ok");
+    while (oks.length > 0) {
+        oks.item(0).parentNode.removeChild(oks.item(0));
+    }
+    fetch(window.location.protocol + "//" + window.location.host.toString() + "/" + route, post).then(function(response) {
         if (response.ok) {
             response.json().then(jsonResponse => {
-                if (jsonResponse["response"]["code"] != "200") {
+                if (checkIfJsonIsError(jsonResponse) && jsonResponse["response"]["code"] != "200") {
                     var error = document.createElement("P");
                     error.innerHTML = jsonResponse["response"]["message"];
                     error.classList.add("error");
                     form.appendChild(error);
+                } else if (checkIfJsonIsError(jsonResponse) && jsonResponse["response"]["code"] == "200") {
+                    var ok = document.createElement("P");
+                    ok.innerHTML = jsonResponse["response"]["message"];
+                    ok.classList.add("ok");
+                    form.appendChild(ok);
                 }
             });
         }
@@ -60,6 +127,18 @@ function setJsonData(url, selectId, setter) {
     }
     xhr.open("GET", url, true);
     xhr.send();
+}
+
+function getJsonData(route) {
+    fetch(window.location.href.toString() + route).then(function(response) {
+        if (response.ok) {
+            response.json().then(jsonResponse => {return jsonResponse});
+        }
+    });
+}
+
+function checkIfJsonIsError(json) {
+    return json.hasOwnProperty("response");
 }
 
 function setSelectList(selectId, options) {
