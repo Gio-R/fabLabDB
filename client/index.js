b4972f9edd4cfbb79c9b3e67112101fd5203243d
Giorgia Rondinini <giorgia.rondinini@gmail.com>
2019-09-30 20:07:49 +0200

    Add js for modify person function

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 window.onload = function() {
     document.getElementById("insert_person").onclick = () => insertPerson();
     document.getElementById("show_people").onclick = () => showPeople();
+    document.getElementById("modify_person").onclick = () => modifyPeople();
 }
 
 /* creates the form and sends the data to insert a new person in the database */
 function insertPerson() {
     clearPage();
     var form = document.getElementById("input_form");
-    form.classList.remove("hidden");
+    showClearElem(form.id);
     var cfInput = createTextInput("cf", "Codice fiscale");
     var nameInput = createTextInput("name", "Nome");
     var surnameInput = createTextInput("surname", "Cognome");
@@ -18,7 +19,7 @@ function insertPerson() {
     form.appendChild(nameInput);
     form.appendChild(surnameInput);
     form.appendChild(button);
-    button.onclick = () => sendInsertData("input_form", "insert_person");
+    button.onclick = () => sendFormData("input_form", "insert_person");
 }
 
 /* creates the list to show the people in the database, with the appropriate filters */
@@ -26,14 +27,63 @@ function showPeople() {
     clearPage();
     var form = document.getElementById("filters_form");
     form.classList.remove("hidden");
-    createFiltersCheckBoxes(form, "people_type", ["all", "Tutti", "people"], ["partners", "Soci", "partners"], ["cutters", "Operatori intagliatrice", "cutterOperators"], ["printers", "Operatori stampante", "printerOperators"]);
+    createFiltersRadioButtons(form, "people_type", ["all", "Tutti", "people"], ["partners", "Soci", "partners"], ["cutters", "Operatori intagliatrice", "cutterOperators"], ["printers", "Operatori stampante", "printerOperators"]);
     setTable("people");
+}
+
+/* show the appropriate forms to modify a person */
+function modifyPeople() {
+    clearPage();
+    var form = document.getElementById("input_form");
+    showClearElem(form.id);
+    fetch(window.location.protocol + "//" + window.location.host.toString() + "/people").then(response => {
+        if (response.ok) {
+            response.json().then(jsonResponse => {
+                if (checkIfJsonIsError(jsonResponse) && jsonResponse["response"]["code"] != "200") {
+                    var error = document.createElement("P");
+                    error.innerHTML = jsonResponse["response"]["message"];
+                    error.classList.add("error");
+                    form.appendChild(error);
+                } else if (!checkIfJsonIsError(jsonResponse)) {
+                    var list = document.createElement("select");
+                    list.name = "cf";
+                    list.id = "cf_select";
+                    form.appendChild(list);
+                    for (const index in jsonResponse) {
+                        var elem = document.createElement("option");
+                        elem.value = jsonResponse[index]._personCf;
+                        elem.innerHTML = jsonResponse[index]._personNome + " " + jsonResponse[index]._personCognome + " -- " + jsonResponse[index]._personCf;
+                        list.appendChild(elem);
+                    }
+                    list.onchange = () => changeCheckedBoxes();
+                    changeCheckedBoxes();
+                    form.appendChild(document.createElement("br"));
+                    createFiltersCheckBoxes(form, ["partner", "true", "Socio"], ["cutter", "true", "Operatore intagliatrice"], ["printer", "true", "Operatore stampante"]);
+                    createHiddenFiltersCheckBoxes(form, ["partner", "false", "Socio"], ["cutter", "false", "Operatore intagliatrice"], ["printer", "false", "Operatore stampante"]);
+                    var okButton = document.createElement("button");
+                    okButton.type = "button";
+                    okButton.innerHTML = "Modifica";
+                    okButton.onclick = () => {
+                        disableCheckBoxes(form);
+                        sendFormData("input_form", "modify_person")
+                    };
+                    form.appendChild(okButton);
+                }
+            });
+        }
+    });
+}
+
+/* fetches data from the given route, and executes the given action */
+function fetchData(route, action) {
+    fetch(window.location.protocol + "//" + window.location.host.toString() + "/" + route).then(action);
 }
 
 /* sets the content of the "result_area" element with the results from the given route */
 function setTable(route) {
     var resultDiv = document.getElementById("result_area");
-    fetch(window.location.protocol + "//" + window.location.host.toString() + "/" + route).then(function(response) {
+    fetchData()
+    fetchData(route, function(response) {
         if (response.ok) {
             response.json().then(jsonResponse => {
                 if (checkIfJsonIsError(jsonResponse) && jsonResponse["response"]["code"] != "200") {
@@ -87,31 +137,98 @@ function showClearElem(elemId) {
     elem.classList.remove("hidden");
 }
 
-/* creates a variable number of checkboxes into the form, with [name, label, route] properties */
-function createFiltersCheckBoxes(form, name, ...checkBoxes) {
-    checkBoxes.forEach(box => {
+/* creates a variable number of radio buttons into the form, with [name, label, route] properties */
+function createFiltersRadioButtons(form, name, ...radioButtons) {
+    radioButtons.forEach(button => {
         var input = document.createElement("input");
         var label = document.createElement("label");
         input.type = "radio";
         input.name = name;
-        input.id = box[0];
-        input.onclick = () => { if (input.checked) { setTable(box[2]); } };
+        input.id = button[0];
+        input.onclick = () => { if (input.checked) { setTable(button[2]); } };
         label.for = input.id;
-        label.innerHTML = box[1];
+        label.innerHTML = button[1];
         form.appendChild(input);
         form.appendChild(label);
         form.appendChild(document.createElement("br"));
     });
 }
 
+/* creates a variable number of checkBoxes into the form, with [name, value, label] properties */
+function createFiltersCheckBoxes(form, ...checkBoxes) {
+    checkBoxes.forEach(box => {
+        var input = document.createElement("input");
+        var label = document.createElement("label");
+        input.type = "checkbox";
+        input.name = box[0];
+        input.value = box[1];
+        input.id = box[0];
+        label.for = input.id;
+        label.innerHTML = box[2];
+        form.appendChild(input);
+        form.appendChild(label);
+        form.appendChild(document.createElement("br"));
+    });
+}
+
+/* creates a variable number of checkBoxes into the form, with [name, value, label] properties, that have the "hidden" class */
+function createHiddenFiltersCheckBoxes(form, ...checkBoxes) {
+    checkBoxes.forEach(box => {
+        var input = document.createElement("input");
+        input.type = "checkbox";
+        input.name = box[0];
+        input.value = box[1];
+        input.checked = true;
+        input.id = box[0] + "_hidden";
+        input.classList.add("hidden");
+        form.appendChild(input);
+    });
+}
+
+/* change the checked boxes based on the value of the "cf_select" element */
+function changeCheckedBoxes() {
+    var list = document.getElementById("cf_select");
+    fetchData("people", response => {
+        if (response.ok) {
+            response.json().then(jsonResponse => {
+                if (!checkIfJsonIsError(jsonResponse)) {
+                    for (const index in jsonResponse) {
+                        if (list.value == jsonResponse[index]._personCf) {
+                            document.getElementById("partner").checked = jsonResponse[index]._personSocio;
+                            document.getElementById("cutter").checked = jsonResponse[index]._personOperatoreIntagliatrice;
+                            document.getElementById("printer").checked = jsonResponse[index]._personOperatoreStampante;
+                        }
+                    }
+                }
+            });
+        }
+    });
+}
+
+/* disables the invisibles checkboxes if the visible ones are checked */
+function disableCheckBoxes(form) {
+    var checkBoxes = document.getElementsByTagName("input");
+    for (const index in checkBoxes) {
+        var box = checkBoxes.item(index);
+        if (box.type == "checkbox" && box.classList.contains("hidden")) {
+            var shownId = box.id.split("_")[0];
+            var shownBox = document.getElementById(shownId);
+            if (shownBox.checked) {
+                box.disabled = true;
+            }
+        }
+    }
+}
+
 /* sends to the said route the content of the given form, giving a visual notice of the result of the operation */
-function sendInsertData(formId, route) {
+function sendFormData(formId, route) {
     var form = document.getElementById(formId);
     var fd = new FormData(form);
     var post = {
         method: "POST",
         body: fd
     };
+    console.log(form);
     var errors = form.getElementsByClassName("error");
     while (errors.length > 0) {
         errors.item(0).parentNode.removeChild(errors.item(0));
@@ -164,41 +281,6 @@ function createTable(tableId, headersId, ...headers) {
     return resultTable;
 }
 
-function setJsonData(url, selectId, setter) {
-    var html_code = "";
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() { 
-        if (xhr.readyState == 4 && xhr.status == 200)
-            setter(selectId, JSON.parse(xhr.responseText));
-    }
-    xhr.open("GET", url, true);
-    xhr.send();
-}
-
-function getJsonData(route) {
-    fetch(window.location.href.toString() + route).then(function(response) {
-        if (response.ok) {
-            response.json().then(jsonResponse => {return jsonResponse});
-        }
-    });
-}
-
 function checkIfJsonIsError(json) {
     return json.hasOwnProperty("response");
-}
-
-function setSelectList(selectId, options) {
-    var selectElement = document.getElementById(selectId);
-    while (selectElement.options.length) {
-        selectElement.remove(0);
-    }
-    for (var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var str = opt._personNome.concat(" ", opt._personCognome, " ", opt._personCf);
-        selectElement.options.add(new Option(str, i));
-    }
-}
-
-function setAllPeopl(selectId) {
-    setJsonData(window.location.href + "people", selectId, setSelectList);
 }
